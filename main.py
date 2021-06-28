672965407d314e24b37a27546087802f25b3fd13
RozeenStha <73871133+RozeenStha@users.noreply.github.com>
2021-06-28 19:21:06 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import discord
+from discord import guild
+from discord import permissions
+from discord import message
 from discord.ext import commands
 import os
 
@@ -22,5 +25,42 @@ async def whoami(ctx) :
 async def clear(ctx, amount=3) :
     await ctx.channel.purge(limit=amount)
 
+@client.command('role')
+@commands.has_permissions(administrator=True) #permissions
+async def role(ctx, user : discord.Member, *, role : discord.Role):
+  if role.position > ctx.author.top_role.position: #if the role is above users top role it sends error
+    return await ctx.send('**:x: | That role is above your top role!**') 
+  if role in user.roles:
+      await user.remove_roles(role) #removes the role if user already has
+      await ctx.send(f"Removed {role} from {user.mention}")
+  else:
+      await user.add_roles(role) #adds role if not already has it
+      await ctx.send(f"Added {role} to {user.mention}") 
+
+@client.command()
+async def mute(ctx, member:discord.Member):
+    role = discord.utils.get(ctx.guild.roles, name="Muted")
+    guild = ctx.guild
+    if role not in guild.roles:
+        perms = discord.Permissions(send_messages=False, speak=False)
+        await guild.create_role(name="Muted", permissions=perms)
+        await member.add_roles(role)
+        await ctx.send(f"User has been unmuted")
+    else:
+       await member.add_roles(role)
+       await ctx.send(f"User has been unmuted")
+
+@client.command()
+async def unmute(ctx, member:discord.Member):
+    role = discord.utils.get(ctx.guild.roles, name="Muted")
+    guild = ctx.guild
+    if role not in guild.roles:
+        perms = discord.Permissions(send_messages=False, speak=False)
+        await guild.create_role(name="Muted", permissions=perms)
+        await member.remove_roles(role)
+        await ctx.send(f"User has been unmuted")
+    else:
+       await member.remove_roles(role)
+       await ctx.send(f"User has been unmuted")
 
 client.run(token)
