9fa5e3bc93464a18ad9148b9383344f33d68778c
RozeenStha <73871133+RozeenStha@users.noreply.github.com>
2021-06-29 10:16:10 +0545

    Update main.py

--- main.py
+++ main.py
@@ -102,4 +102,8 @@ async def snipe(ctx):
 
     await ctx.channel.send(embed=embed)
 
+@client.command()
+async def av(ctx, member:discord.Member):
+ await ctx.send(member.avatar_url)
+
 client.run(token)
